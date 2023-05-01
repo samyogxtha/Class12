34f0f08f13d9496f37fb893db8036e7e82eaca78
Samyog Shrestha <samyogxtha2006@gmail.com>
2023-05-01 19:40:41 +0400

    update

---
+++ 17.login.py
@@ -0,0 +1,36 @@
+login_details = {'Samyog':'samyog123','Idris':'idris123','Hussain':'hussain123'}
+
+def signup():
+    while True:
+        username = input('Enter username: ')
+        if username in login_details:
+            print('The user already Exists.')
+            continue
+        password = input('Enter password: ')
+        login_details[username] = password
+        break
+
+def login():
+    while True:
+        username = input('Enter username: ')
+        if username not in login_details:
+            print('User not Found.')
+            continue
+        password = input('Enter password: ')
+        if login_details[username] == password:
+            print('Log In Successful!')
+        else:
+            print('Invalid Password')
+        break
+
+def main():
+    while True:
+        print('1. Sign In\n2. Log In')
+        qn = int(input('Enter Choice: '))
+        if qn == 1:
+            signup()
+        else:
+            login()
+
+if __name__ == '__main__':
+    main()
