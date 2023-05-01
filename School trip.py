0add9d96ff7dee28ff83240ef04c318d854a531c
Samyog Shrestha <samyogxtha2006@gmail.com>
2023-05-01 17:55:16 +0400

    update School trip.py

--- School trip.py
+++ School trip.py
@@ -1,14 +1,17 @@
 def main():
-
-
-    cost_coach = 550
-    cost_ticket = 30
     while True:
         n_students = int(input('Enter the number of Students: '))
         if 0 <= n_students <= 45:
             break
         else:
             print('The Maximum number of students is 45.')
+    cost_ticket = (550/n_students)+((n_students*30)/(n_students-n_students//10))
+    print('The cost of Ticket per Student: $',cost_ticket)
+
+
+    name_list = {}
+    for i in range(n_students)
+
 
 if __name__ == '__main__':
     main()
