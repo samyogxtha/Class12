b16d5c0a807339878c639457c17a2b7e68a5a372
Samyog Shrestha <samyogxtha2006@gmail.com>
2023-05-01 17:27:40 +0400

    update School trip.py

--- School trip.py
+++ School trip.py
@@ -1,1 +1,13 @@
- replit
+def main():
+
+
+    cost_coach = 550
+    cost_ticket = 30
+    while True:
+        n_students = int(input('Enter the number of Students: '))
+        if 0 <= n_students <= 45:
+            break
+
+
+if __name__ == '__main__':
+    main()
