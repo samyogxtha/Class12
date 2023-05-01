521383157a15bbdc36db78b7ebeffc07698c87a6
Samyog Shrestha <samyogxtha2006@gmail.com>
2023-05-01 18:15:38 +0400

    update

--- School trip.py
+++ School trip.py
@@ -7,11 +7,24 @@ def main():
             print('The Maximum number of students is 45.')
     cost_ticket = (550/n_students)+((n_students*30)/(n_students-n_students//10))
     print('The cost of Ticket per Student: $',cost_ticket)
+    print('\n\n')
 
 
     name_list = {}
-    for i in range(n_students)
-
+    for i in range(n_students):
+        name = input('Enter Name of Student: ')
+        paid = input('Money Paid? (y/n)')
+        if paid in 'yY':
+            paid = True
+        else:
+            paid = False
+        name_list[name] = paid
+        print('\n')
+    print('Name\t\t|\t Paid')
+    for i in name_list:
+        print(i,'\t\t|\t',name_list[i])
+    
+            
 
 if __name__ == '__main__':
     main()
