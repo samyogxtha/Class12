51c7d82797984b3008d548f8c79b3ae507aee5ec
Samyog Shrestha <samyogxtha2006@gmail.com>
2023-05-03 21:09:50 +0400

    update

--- 19.bfunction.py
+++ 19.bfunction.py
@@ -1,14 +1,12 @@
 def bfunction(list,chk_no):
     list.sort()
-    min = 0
-    max = len(list) - 1
-    mid = (min + max) // 2
-
+    min,max = 0,len(list) - 1
     while min <= max:
+        mid = (min + max) // 2
         if list[mid] == chk_no:
             return mid
         elif list[mid] < chk_no:
-            max = mid + 1
+            min = mid + 1
         else:
             max = mid - 1
     return 'Not Found'
@@ -18,6 +16,7 @@ def main():
     n_list = int(input('Enter the number of elements in List: '))
     for i in range(n_list):
         list.append(int(input('Enter Number: ')))
+    print('\nList: ',list)
     number = int(input('Enter Number to Search: '))
     index_of_number = bfunction(list,number)
     print('The index of Number in List is: ',index_of_number)
