730bf9131c76dbb895ba365044a3a590179f650e
samyo <samyogxtha2006@gmail.com>
2023-06-13 22:54:03 +0400

    sqlconn

--- sql/sqlcon.py
+++ sql/sqlcon.py
@@ -1,22 +1,33 @@
 import mysql.connector as msconn
 
-def main():
-    sqlcon = msconn.connect(host = 'localhost', user = 'root', passwd = 'root', database = 's_school')
-    mycur =  sqlcon.cursor()
-    print()
+sqlcon = msconn.connect(host = 'localhost', user = 'root', passwd = 'root', database = 's_school')
 
+def main():
+    print('\n\tMENU DRIVEN PROGRAM\n\n\
+      1. Display all Records\n\
+      2. Search for an Entry\n\
+      3. Insert an Entry\n\
+      4. Modify an Entry\n\
+      5. Delete an Entry\n')
+    search()
 
 def display():
-    global mycur
-    mycur.execute('select * from employee')
-    mydata = mycur.fetchall()
-    n_rec = mycur.rowcount
-    print('the tot records are',n_rec)
+    cur_disp =  sqlcon.cursor()
+    cur_disp.execute('select * from employee')
+    mydata = cur_disp.fetchall()
+    n_rec = cur_disp.rowcount
+    print('The Total records are',n_rec)
     for row in mydata:
         print(row)
 
 def search():
-    pass
+    print('Search by Name or ID Number.')
+    search = insert('Enter Name or ID: ')
+    cur_search = sqlcon.cursor()
+    cur_search.execute('select * from employee where name = "'+search+'"')
+    mydata = cur_search.fetchall()
+    for row in mydata:
+        print(row)
 
 def insert():
     pass
