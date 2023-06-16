6d78c54b4baee3da46b28fbfde02d86f9e26076a
samyo <samyogxtha2006@gmail.com>
2023-06-16 17:59:37 +0400

    w

--- tmp2.py
+++ tmp2.py
@@ -1,7 +1,12 @@
-fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+def manipulate_data(input_string, input_tuple, input_list):
+    step_1 = input_string + input_tuple[0]
+    step_2 = step_1 + len(input_list)
+    step_3 = list(step_2)
+    step_4 = step_3[::-1]
+    return step_4
 
-(green, *yellow,red) = fruits
-
-print(green)
-print(yellow)
-print(red)
+# Testing the function
+string = "Hello"
+tuple_data = ("World", "Python", "Programming")
+list_data = [1, 2, 3, 4]
+print(manipulate_data(string, tuple_data, list_data))
